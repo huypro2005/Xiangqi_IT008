4d777be779506ececd3ad489b4d4f56cd9229528
Cao Thành Huy <145009627+huypro2005@users.noreply.github.com>
2026-01-11 13:03:11 +0700

    up

--- WinFormsApp1/obj/Release/net9.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/obj/Release/net9.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2733a2999fdfa6385d010a9047bff9695b961db1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91f200faa77afb7c446cc878ce11861ce4286299")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
